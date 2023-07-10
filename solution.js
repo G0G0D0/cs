6465f13ab43b80678d6c03aaa5707860d99159ef
G0G0D0 <95535369+G0G0D0@users.noreply.github.com>
2023-07-10 12:37:46 +0530

    Add files via upload

---
+++ solution.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Initialize the carousel
+    var testimonialCarousel = new bootstrap.Carousel(document.getElementById('testimonialCarousel'));
+  
+    // Set the carousel to slide every 5 seconds
+    testimonialCarousel.interval = 5000;
+  });
+  
